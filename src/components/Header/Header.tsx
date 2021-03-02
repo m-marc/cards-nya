760a4cbf424c0a69c890fc6f63895e8d98cbfe3d
m-marc <marchenkovmaxim@gmail.com>
2021-03-02 15:11:23 +0300

    Separated Header css module

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {PATH} from "../../routes/Routes";
 import {NavLink} from "react-router-dom";
+import s from "./Header.module.css"
 
 type Props = {}
 
 export const Header = (props: Props) => {
     return (
-        <header className={`header`}>
+        <header className={s.header}>
             <NavLink to={PATH.TEST} activeClassName={"active"}>Home</NavLink>
             <NavLink to={PATH.PROFILE} activeClassName={"active"}>Profile</NavLink>
             <NavLink to={PATH.LOGIN} activeClassName={"active"}>Login</NavLink>
