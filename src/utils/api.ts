8e4d9f6280b630811235fea8d9aafc3d2d08f737
viktoriabakun <viktoriabakun00@gmail.com>
2021-03-05 23:39:05 +0300

    add register api #4

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,4 +4,5 @@ const instance = axios.create({baseURL:"http://localhost:7542/2.0/"})
 
 export const AuthAPI = {
 
+    register: (email: string, password: string) =>  instance.post(`auth/register`, {email, password})
 }
