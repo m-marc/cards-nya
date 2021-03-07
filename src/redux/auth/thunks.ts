f53966c339d170019c83a2558a3395db51e8be8e
Anton <anton.schogolev@gmail.com>
2021-03-07 15:23:24 +0300

    Thunk loginTC #3

--- src/redux/auth/thunks.ts
+++ src/redux/auth/thunks.ts
@@ -1,3 +1,19 @@
 import {Dispatch} from "redux";
+import {AuthAPI} from "../../utils/api";
+import {loginAC, setErrorAC} from "./actions";
 
-export const auth = () => (dispatch: Dispatch) => {}
+export const auth = () => (dispatch: Dispatch) => {
+}
+
+export const loginTC = (login: string, password: string, rememberMe: boolean) =>
+    async (dispatch: Dispatch) => {
+        try {
+            const response = await AuthAPI.login(login, password, rememberMe)
+            dispatch(loginAC(response.data))
+        } catch (e) {
+            const error = e.response
+                ? e.response.data.error
+                : (e.message + ', more details in the console');
+            dispatch(setErrorAC(error))
+        }
+    }
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,24 @@
 import axios from 'axios'
 
+type LoginResponseType = {
+    _id: string;
+    email: string;
+    name: string;
+    avatar?: string;
+    publicCardPacksCount: number; // количество колод
+
+    created: Date;
+    updated: Date;
+    isAdmin: boolean;
+    verified: boolean; // подтвердил ли почту
+    rememberMe: boolean;
+
+    error?: string;
+}
+
 const instance = axios.create({baseURL:"http://localhost:7542/2.0/"})
 
 export const AuthAPI = {
-
+    login: (login: string, password: string, rememberMe: boolean) =>
+        instance.post<LoginResponseType>("auth/login",{email: login,password,rememberMe})
 }
