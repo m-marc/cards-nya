61ecfd1ae28f4411c14215b9a3d5eb0c2c5fe57c
m-marc <marchenkovmaxim@gmail.com>
2021-03-08 19:46:16 +0300

    #5 Styled reset button async requests

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,9 +2,30 @@ import axios from 'axios'
 
 const instance = axios.create({baseURL:"http://localhost:7542/2.0/"})
 
+const btnStyle = `border-radius: 3px;
+    background: #3aa54c;
+    color: #fff;
+    display: block;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 1.25em;
+    margin: 24px 0;
+    padding: 10px 18px;
+    text-decoration: none;
+    width: 180px;
+    text-align: center;`
+
 export const AuthAPI = {
-    forgot:(email: string, from: string, message: string) =>
-        axios.post(`https://neko-back.herokuapp.com/2.0/auth/forgot`,{email, from, message}),
-    newpass:(password: string, resetPasswordToken: string) =>
-        instance.post(`auth/set-new-password`,{password, resetPasswordToken})
+    forgot: async (email: string) => {
+        const response = await axios.post(`https://neko-back.herokuapp.com/2.0/auth/forgot`, {
+            email,
+            message: `<div style="font-size: 16px">Click the link below and you'll be redirected to a site where you can set a new password</div>
+                <div><a style="${btnStyle}" href='http://localhost:3000/#/newpassword/$token$'>Set new password</a></div>`
+        })
+        return response.data
+    },
+    newpass: async (password: string, resetPasswordToken: string) => {
+        const response = await instance.post(`auth/set-new-password`, {password, resetPasswordToken})
+        return response.data
+    }
 }
