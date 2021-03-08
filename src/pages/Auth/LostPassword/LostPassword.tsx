e4ad141faba9aed0483e12067fa46ef782b70cd7
m-marc <marchenkovmaxim@gmail.com>
2021-03-08 20:46:39 +0300

    #5 Initial UI render

--- src/pages/Auth/LostPassword/LostPassword.tsx
+++ src/pages/Auth/LostPassword/LostPassword.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useHistory} from "react-router-dom";
 import SuperButton from "../../../components/SuperButton/SuperButton";
 import SuperInput from "../../../components/SuperInput/SuperInput";
@@ -6,26 +6,35 @@ import {useDispatch, useSelector} from "react-redux";
 import {thunkForgotPassword} from "../../../redux/auth/thunks";
 import {selectApp} from "../../../redux/Selectors";
 import {Loader} from "../../../components/Loader/Loader";
+import {setError} from "../../../redux/main/appActions";
 
 
 export const LostPassword = () => {
     const history = useHistory()
     const backHome = () => history.push("/")
     const [email, setEmail] = useState<string>("")
-    const [validation, setValidation] = useState("")
+    const [validation, setValidation] = useState<string>("")
+    const [initial, setInitial] = useState<boolean>(true)
     const {error, isLoading, success} = useSelector(selectApp)
     const dispatch = useDispatch()
     const regExp = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i
 
     const resetPasswordHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault()
-        if (email === '' || !email.match(regExp)) {
+        if (!email.match(regExp)) {
             setValidation('Email should be valid!')
         } else {
             setValidation('')
             dispatch(thunkForgotPassword(email))
         }
     }
+
+    useEffect(() => {
+        if (initial) {
+            (success || error) && dispatch(setError(""))
+            setInitial(false)
+        }
+    }, [initial, success, error, setInitial, dispatch])
 
     return <>
         <h1>Reset password</h1>
