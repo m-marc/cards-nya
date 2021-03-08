a2b5ebfa598f9eb35e27f326e49434358325f021
m-marc <marchenkovmaxim@gmail.com>
2021-03-08 19:43:37 +0300

    Common app actions & reducers & selectors

--- src/redux/Selectors.ts
+++ src/redux/Selectors.ts
@@ -4,3 +4,4 @@ interface IRootState extends IGlobalState{}
 
 export const selectProfile = (state: IRootState) => state.profile
 export const selectAuth = (state: IRootState) => state.auth
+export const selectApp = (state: IRootState) => state.app;
